9568035de9d7b4501f45eea7c7e70b9840888798
Chocky._18 <75876430+chocky18@users.noreply.github.com>
2024-06-21 01:37:53 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,13 +124,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static'),
+    "portfolio/portfolio/static",
 ]
 
 # Ensure STATIC_ROOT is defined and set correctly for production deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Ensure ALLOWED_HOSTS includes your Render domain
 ALLOWED_HOSTS = ['yourdomain.com', 'portfolio-xxx5.onrender.com']

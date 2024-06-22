edd552bdef890e5d83d11f953aedf47efcb43530
Chocky._18 <75876430+chocky18@users.noreply.github.com>
2024-06-22 09:48:23 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['yourdomain.com', 'localhost', 'portfolio-d734.onrender.com', '
 
 INSTALLED_APPS = [
     'portfolio',
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,15 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # settings.py
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     "portfolio/portfolio/static",
-# ]
+STATICFILES_DIRS = [
+    "/static/",
+]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 # Ensure STATIC_ROOT is defined and set correctly for production deployment
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Ensure ALLOWED_HOSTS includes your Render domain
 ALLOWED_HOSTS = ['yourdomain.com', 'portfolio-xxx5.onrender.com', '.vercel.app']
